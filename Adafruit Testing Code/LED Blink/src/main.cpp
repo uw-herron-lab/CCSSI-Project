788396214b45bd0e2903b691386173dda89f0f73
Detadja <denisetanumihardja@yahoo.com>
2025-04-08 22:22:16 -0700

    Builtin and Neopixel LED tests

--- Adafruit Testing Code/LED Blink/src/main.cpp
+++ Adafruit Testing Code/LED Blink/src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600); 
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Built-In LED Blinking Testing
+  Serial.println("In-built LED On!");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  Serial.println("In-built LED Off!");
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
 }
